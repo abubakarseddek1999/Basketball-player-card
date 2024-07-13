5fcd36572b32505478a5f1abe040d2bb3e2e4f21
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-07-13 22:08:55 +0600

    make navbar and footer

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,7 +2,7 @@ import BasketballPlayerCards from "../../../Components/BasketballPlayerCard/Bask
 
 const Home = () => {
     return (
-        <div className="p-5">
+        <div className="p-5 pt-12">
             <p className=" text-4xl md:text-6xl font-bold pt-10  text-center mb-10"> Basketball Players</p>
             <BasketballPlayerCards></BasketballPlayerCards>
         </div>
