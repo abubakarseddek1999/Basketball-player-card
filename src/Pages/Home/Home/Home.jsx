4139d8b1326726792fee53d1d03c31cbe18f393f
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-07-13 21:21:10 +0600

    basic setup project

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <p className="min-h-screen text-4xl md:text-6xl font-bold pt-10  text-center">React Basketball Player Card</p>
+        </div>
+    );
+};
+
+export default Home;
